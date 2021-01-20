2bd556f7a73ca528ac62e9af7f88b7c7e3c27806
Reid <rlj0713@gmail.com>
2021-01-20 17:56:29 -0500

    commit tree 375993983c1aaf3deeae1b61543037439f545a1e

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+
+class Dog
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    
+    def breed=(breed)
+        @dog = breed
+    end
+
+    def name
+        @breed
+    end
+
+
+
+    def name=(name)
+        @dog = name
+    end
+
+    def name
+        @name
+    end
+end
+
